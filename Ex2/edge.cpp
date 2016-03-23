8447ec6835012b967b32eb5bff4183ff2317b7bf
camouflage <363419254@qq.com>
2016-03-23 17:32:18 +0800

    Add line and point.

--- Ex2/edge.cpp
+++ Ex2/edge.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <cstdio>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
+
+struct myLine {
+    double m;
+    double c;
+    bool vertical;
+};
+
+bool cmp(const Vec2f& a, const Vec2f& b) {
+    return a[1] < b[1];
+}
 
 // Ref: http://docs.opencv.org/2.4/doc/tutorials/introduction/linux_gcc_cmake/linux_gcc_cmake.html#linux-gcc-usage
 int main(int argc, char* argv[]) {
@@ -13,7 +24,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    Mat srcImg, dstImg, srcGray, edge;
+    Mat srcImg, dstImg, srcGray, srcBlur, edge;
     srcImg = imread(argv[1]);
     if ( !srcImg.data ) {
         cout << "No image data" << endl;
@@ -21,24 +32,88 @@ int main(int argc, char* argv[]) {
     }
 
     // Ref: http://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#resize
+    // 
     resize(srcImg, srcImg, Size(), 0.2, 0.2);
 
     // Ref: http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
     cvtColor(srcImg, srcGray, CV_BGR2GRAY);
 
-    blur(srcGray, edge, Size(3, 3));
+    blur(srcGray, srcBlur, Size(3, 3));
 
     int lowThreshold = 125;
-    int ratio = 3;
+    int highThreshold = 500;
     int kernel_size = 3;
-    Canny(edge, edge, lowThreshold, lowThreshold * ratio, kernel_size);
+    Canny(srcBlur, edge, lowThreshold, highThreshold, kernel_size);
+
 
     dstImg.create(srcImg.size(), srcImg.type());
     dstImg = Scalar::all(0);
 
-    srcImg.copyTo(dstImg, edge);
+    // Ref: http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_lines/hough_lines.html
+    float rho = 1;
+    float theta = CV_PI / 100;
+    vector<Vec2f> lines;
+    vector<myLine> mls;
+    HoughLines(edge, lines, rho, theta, 97, 0, 0);
+    sort(lines.begin(), lines.end(), cmp);
+
+    for ( size_t i = 0; i < lines.size(); i++ ) {
+        float rho = lines[i][0], theta = lines[i][1];
+
+        // Eliminate duplicates.
+        float deltaRho = 50;
+        float deltaTheta = 0.25;
+        cout << rho << " " << theta << endl;
+        if ( i > 0 && abs(rho - lines[i - 1][0]) < deltaRho && abs(theta - lines[i - 1][1]) < deltaTheta ) {
+            continue;
+        }
+
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a * rho, y0 = b * rho;
+        pt1.x = cvRound(x0 + 1000 * (-b));
+        pt1.y = cvRound(y0 + 1000 * (a));
+        pt2.x = cvRound(x0 - 1000 * (-b));
+        pt2.y = cvRound(y0 - 1000 * (a));
+        line(dstImg, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+
+        struct myLine ml;
+        // Deal with lines parallel to Y axis.
+        if ( b == 0 ) {
+            ml.vertical = 1;
+            ml.c = rho;
+            printf("Line equation: x = %lf\n", rho);
+        } else {
+            ml.vertical = 0;
+            double m = -a / b, c = rho / b;
+            ml.m = m;
+            ml.c = c;
+            printf("Line equation: y = %lfx + %lf\n", m ,c);
+        }
+        
+        mls.push_back(ml);
+    }
+
+    // Calculate line-line intersection.
+    // Ref: https://en.wikipedia.org/wiki/Line–line_intersection
+    for ( int i = 0; i < 2; ++i ) {
+        for ( int j = 2; j < 4; ++j ) {
+            double a = mls[i].m, b = mls[j].m;
+            double c = mls[i].c, d = mls[j].c;
+            double x, y;
+            if ( mls[i].vertical == 1 ) {
+                x = c;
+                y = b * c + d;
+            } else {
+                x = (d - c) / (a - b);
+                y = (a * d - b * c) / (a - b);
+            }
+            printf("Point: (%lf, %lf)\n", x, y);
+        }
+    }
 
     namedWindow("Edge", CV_WINDOW_AUTOSIZE);
+    // imshow("Display Image", edge);
     imshow("Display Image", dstImg);
 
     waitKey(0);
