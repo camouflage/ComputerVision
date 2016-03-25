42a70482d426b50531aa670e2635bbe144077fa6
camouflage <363419254@qq.com>
2016-03-25 10:43:28 +0800

    Add report.

--- Ex2/edge.cpp
+++ Ex2/edge.cpp
@@ -7,12 +7,22 @@
 using namespace cv;
 using namespace std;
 
+// Store line info
 struct myLine {
-    double m;
-    double c;
-    bool vertical;
+    double m; // Slope of the line
+    double c; // Intercept of the line
+    bool vertical; // True if the line is parallel to y-axis
 };
 
+/*
+void myHoughLines(const Mat& img, vector<Vec2f>& lines, float rho, float theta, int threshold) {
+    float irho = 1 / rho;
+    int width = img.size().width;
+    int height = img.size().height;  
+}
+*/
+
+// Compare function based on theta
 bool cmp(const Vec2f& a, const Vec2f& b) {
     return a[1] < b[1];
 }
@@ -32,30 +42,37 @@ int main(int argc, char* argv[]) {
     }
 
     // Ref: http://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#resize
-    // 
     resize(srcImg, srcImg, Size(), 0.2, 0.2);
 
     // Ref: http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
+    // Transfrom to gray scale.
     cvtColor(srcImg, srcGray, CV_BGR2GRAY);
 
+    // Blur the image to erase some noise
     blur(srcGray, srcBlur, Size(3, 3));
 
+    // Set threshold for Canny
     int lowThreshold = 125;
     int highThreshold = 500;
     int kernel_size = 3;
     Canny(srcBlur, edge, lowThreshold, highThreshold, kernel_size);
 
+    // Ref: http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_lines/hough_lines.html
+    // Set parameters for hough transform
+    float rho = 1;
+    float theta = CV_PI / 100;
+    int threshold = 97;
+    vector<Vec2f> lines;
+    vector<myLine> mls;
+
+    HoughLines(edge, lines, rho, theta, threshold, 0, 0);
+    // myHoughLines(edge, lines, rho, theta, threshold);
+    
+    // Sort lines based on theta
+    sort(lines.begin(), lines.end(), cmp);
 
     dstImg.create(srcImg.size(), srcImg.type());
     dstImg = Scalar::all(0);
-
-    // Ref: http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_lines/hough_lines.html
-    float rho = 1;
-    float theta = CV_PI / 100;
-    vector<Vec2f> lines;
-    vector<myLine> mls;
-    HoughLines(edge, lines, rho, theta, 97, 0, 0);
-    sort(lines.begin(), lines.end(), cmp);
 
     for ( size_t i = 0; i < lines.size(); i++ ) {
         float rho = lines[i][0], theta = lines[i][1];
@@ -63,11 +80,12 @@ int main(int argc, char* argv[]) {
         // Eliminate duplicates.
         float deltaRho = 50;
         float deltaTheta = 0.25;
-        cout << rho << " " << theta << endl;
+        // cout << rho << " " << theta << endl;
         if ( i > 0 && abs(rho - lines[i - 1][0]) < deltaRho && abs(theta - lines[i - 1][1]) < deltaTheta ) {
             continue;
         }
 
+        // Produce two lines on the line
         Point pt1, pt2;
         double a = cos(theta), b = sin(theta);
         double x0 = a * rho, y0 = b * rho;
@@ -78,13 +96,14 @@ int main(int argc, char* argv[]) {
         line(dstImg, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
 
         struct myLine ml;
-        // Deal with lines parallel to Y axis.
+        // Deal with lines parallel to y-axis.
         if ( b == 0 ) {
             ml.vertical = 1;
             ml.c = rho;
             printf("Line equation: x = %lf\n", rho);
         } else {
             ml.vertical = 0;
+            // m for slope and c for y-intercept
             double m = -a / b, c = rho / b;
             ml.m = m;
             ml.c = c;
@@ -112,7 +131,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    namedWindow("Edge", CV_WINDOW_AUTOSIZE);
+    // namedWindow("Edge", CV_WINDOW_AUTOSIZE);
     // imshow("Display Image", edge);
     imshow("Display Image", dstImg);
 
--- Ex2/execute.sh
+++ Ex2/execute.sh
@@ -1,2 +1,2 @@
 g++ edge.cpp `pkg-config --cflags --libs opencv`
-./a.out ./Dataset/6.jpg
+./a.out ./Dataset/1.jpg
